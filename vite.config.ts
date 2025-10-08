23301451045ffc4069b2aae061dc7cc1f7009b9f
Guirado Léo <horotopia@hotmail.com>
2025-10-08 10:35:41 +0200

    Add GitHub Actions workflow for automated deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/https://github.com/M0rPH3U53/M0rPH3U53.github.io.git/',
+  base: '/',
   plugins: [react()],
 })
