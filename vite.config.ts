9f24b8abd076f36fc762734eb5f75579afe279db
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2025-10-08 11:45:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://M0rPH3U53.github.io/',
+  base: 'https://m0rph3u53.github.io/',
   plugins: [react()],
 })
