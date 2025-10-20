3860e175bde6b8425cef85b577a02782785309df
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2025-10-20 16:25:40 +0200

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,7 +10,9 @@ const Contact: React.FC = () => {
         
         <div className="contact-info">
           <div className="contact-item">
-            <div className="contact-icon">📧</div>
+            <div className="contact-icon">
+            <img src="/proton.svg" alt="Email Contact Icon" />
+            </div>
             <h3 className="text-white font-semibold mb-2">Email</h3>
             <p className="text-primary-500">locatelli.r@protonmail.ch</p>
           </div>
@@ -28,7 +30,9 @@ const Contact: React.FC = () => {
           </div>
           
           <div className="contact-item">
-            <div className="contact-icon">🗺️</div>
+            <div className="contact-icon">
+            <img src="/localisation.png" alt="Localisation Contact Icon" />
+            </div>
             <h3 className="text-white font-semibold mb-2">Localisation</h3>
             <p className="text-primary-500">Paris, France</p>
           </div>
